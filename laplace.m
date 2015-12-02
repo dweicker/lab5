98a48ae7167a2dad84533f2254543825c8bccb87
FlorentinG <florentin.goyens@student.uclouvain.be>
2015-12-02 19:36:02 +0100

    flo: hate the payer not the game

--- laplace.m
+++ laplace.m
@@ -5,27 +5,27 @@
 % LAB 5
 
 close all;
-hold on;
+format long;
 
 % Solution with h = 0.2
  N = 19; M = 11; 
  U02 = sol(M,N);
  X = [0 4]; Y = [0 2]; 
+ figure();
  imagesc(X,Y,U02);  title('Solution with h = 0.2');
+ colorbar(); xlabel('x'); ylabel('y');
  Tmiddel02 = U02(7,11); % U is 13x21
- fprintf('T(2,1) = %f for h = 0.2 \n',Tmiddel02);
+ fprintf('T(2,1) = %4.16f for h = 0.2 \n',Tmiddel02);
  
 % Solution with h = 0.1
  N = 39; M = 21; 
  U01 = sol(M,N);
- X = [0 4]; Y = [0 2]; 
+ X = [0 4]; Y = [0 2];
+ figure();
  imagesc(X,Y,U01);  title('Solution with h = 0.1');
- Tmiddel01 = U01(12,21); % U02 is 23x41 
- fprintf('T(2,1) = %f for h = 0.1 \n',Tmiddel01);
- 
- 
- 
- 
+ colorbar(); xlabel('x'); ylabel('y');
+ Tmiddel01 = U01(12,21); % U is 23x41
+ fprintf('T(2,1) = %4.16f for h = 0.1 \n',Tmiddel01);
  
  
  
--- sol.m
+++ sol.m
@@ -6,22 +6,25 @@ function U = sol(M,N)
 
  n = N*(M+2);% size of system
  e = ones(n,1);
- A = spdiags([-e -e 4*e -e -e],[-N -1 0 1 N],n,n);
+ band = M + 2;
+ A = spdiags([-e -e 4*e -e -e],[-band -1 0 1 band],n,n);
+ 
  %1) add known values on sides in term b
  b = zeros(n,1);
- b(1:N:end) = 300;
- b(N:N:end) = 600;
- for i = 0:M
-    A(N+1+i*N, N+1+i*N-1) = 0; % left side of domain
-    A(N+i*N, N+i*N +1) = 0;% right side of domain
- end
- %2) correct lower and upper equations with ghost points etc
- for i = 1:N 
-    A(i,i+N) = -2;
-    A(end-i+1, end-i+1-N) = -2;
- end
+ b(1:M+2) = 300;
+ b(((N-1)*(M+2)+1):end) = 600;
+ 
+ % 2) Fix ghost points and some coefficients
+ for i = 1:N-1
+    A(M+2 + (i-1)*(M+2),M+2 + (i-1)*(M+2) +1) = 0; % upper side of domain
+    A(M+2 + (i-1)*(M+2),M+2 + (i-1)*(M+2) -1) = -2;% upper side of domain
+    A(1+i*(M+2), 1+i*(M+2) -1) = 0;% lower side of domain
+    A(1+i*(M+2), 1+i*(M+2) +1) = -2;% lower side of domain
+ end 
+ A(N*(M+2), N*(M+2) -1) = -2;
+ A(1,2) = -2;
 
 U = A\b;
-U = reshape(U,N,M+2)';
+U = reshape(U,M+2,N);
 U = [300*ones(M+2,1) U 600*ones(M+2,1)];
 end
